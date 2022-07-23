2b9732ae9585832ef4cbb2f838708bcd8f5db2bc
twills <treav.wills@gmail.com>
2022-07-22 19:22:31 -0600

    clear all variables and allow player to play again

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,12 @@ var j = 0;
 localStorage.setItem("localQuestionIndex", j);
 var points = 0;
 localStorage.setItem("localPoints", points);
-var timer = 1;
+var timer = 2;
 localStorage.setItem("localTimer", timer);
 var scores = [0];
 localStorage.setItem("localScores", JSON.stringify(scores));
+var gameCount = 1;
+localStorage.setItem("localGameCount", gameCount);
 
 var quizQuestionsArray = [
     ["What is your favorite Star Wars Quote?", "Hello there!", "I've got a bad feeling about this!", "GENERAL KENOBI!", "May the force be with you", "Any"],
@@ -14,10 +16,10 @@ var quizQuestionsArray = [
     ["What is the correct JavaScript syntax to change the content of the following HTML element: <p id=\"demo\">This is a demonstration.</p>","document.getElement(\"p\").innerHTML = \"Hello World!\";","#demo.innerHTML=\"Hello World!\";","document.getElementById(\"demo\").innerHTML=\"Hello World!\";","document.getElementByName(\"p\").innerHTML=\"Hello World!\";","document.getElementById(\"demo\").innerHTML=\"Hello World!\";"],
     ["Where is the correct place to insert a JavaScript?","The <body> section","The <head> section","Both the <head> section and the <body> section are correct","In the <nav> section","The <body> section"],
     ["What is the correct syntax for referring to an external script called \"xxx.js\"?","<script src=\"xxx.js\">","<script name=\"xxx.js\">","<script href=\"xxx.js\">","<script nav=\"xxx.js\">","<script src=\"xxx.js\">"],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
+    ["Placeholder placeholder placeholder placeholder","answer answer answer answer","answer answer answer answer","answer answer answer answer","answer answer answer answer",""],
+    ["Placeholder placeholder placeholder placeholder","answer answer answer answer","answer answer answer answer","answer answer answer answer","answer answer answer answer","Any"],
+    ["Placeholder placeholder placeholder placeholder","answer answer answer answer","answer answer answer answer","answer answer answer answer","answer answer answer answer","Any"],
+    ["Placeholder placeholder placeholder placeholder","answer answer answer answer","answer answer answer answer","answer answer answer answer","answer answer answer answer","Any"],
 ];
 
 
@@ -43,19 +45,38 @@ var option2 = document.createElement("button");
 var option3 = document.createElement("button");
 var option4 = document.createElement("button");
 
+// create leaderboard elemtns
+var leaderBoard = document.createElement("p");
+leaderBoard.className = "timer leaderBoard";
+leaderBoard.innerText = "Leaderboard";
+var table = document.createElement("table");
+table.className = "question table";
+var playAgainButton = document.createElement("button");
+playAgainButton.className = "answers playAgainButton";
+playAgainButton.textContent = "Play Again!"
+playAgainButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    playAgain();
+});
+
 // create the form elements
 var initialsForm = document.createElement("form");
 var initialsFormText = document.createElement("input");
 var initialsFormSubmit = document.createElement("input");
 var initialsFormLabel = document.createElement("label");
+var quizTimerTimesUp = document.createElement("p");
 
 // add types, IDs and Labels to the form elements
+initialsForm.className = "answers";
 initialsFormText.type = "text";
 initialsFormSubmit.type = "submit";
 initialsFormText.id = "fInitials";
 initialsFormLabel.setAttribute("for", "fInitials");
 initialsFormLabel.innerText = "Initials: ";
 initialsFormSubmit.addEventListener("click", saveScore);
+quizTimerTimesUp.innerText = "Times Up!";
+
 
 // append the form together
 initialsForm.appendChild(initialsFormLabel);
@@ -105,11 +126,9 @@ function playGame() {
     var secondsLeft = localStorage.getItem("localTimer");
     var questionIndex = localStorage.getItem("localQuestionIndex");
     var timerInterval = setInterval(function() {
-        // quizTimerP.textContent = secondsLeft + " seconds left!";
-        // secondsLeft--;
         quizTimerP.textContent = localStorage.getItem("localTimer") + " seconds left!";
         localStorage.setItem("localTimer", --secondsLeft);
-        if(localStorage.getItem("localTimer") == 0) {
+        if(localStorage.getItem("localTimer") == -1) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
             quizEnd();
@@ -157,9 +176,14 @@ function getAnswer (questionIndex) {
 };
 
 function quizEnd() {
-    quizTimerP.textContent = "Times up!";
+    quizTimerDiv.removeChild(quizTimerP);
+    quizTimerDiv.appendChild(quizTimerTimesUp);
     quizQuestionP.textContent = "Nice work! Enter your initials to save your score."
-    quizDiv.removeChild(quizAnswersDiv);
+    quizAnswersDiv.removeChild(option1);
+    quizAnswersDiv.removeChild(option2);
+    quizAnswersDiv.removeChild(option3);
+    quizAnswersDiv.removeChild(option4);
+
     quizDiv.appendChild(initialsForm);
 }
 
@@ -178,5 +202,58 @@ function saveScore() {
     localStorage.setItem("localScores", JSON.stringify(tempScores));
     let poop = JSON.parse(localStorage.getItem("localScores"));
     console.log(poop);
+
+    displayLeaderboard();
 }
 
+function displayLeaderboard() {
+    scores = JSON.parse(localStorage.getItem("localScores"));
+    console.log(scores);
+    quizDiv.removeChild(initialsForm);
+    quizQuestionDiv.removeChild(quizQuestionP);
+    quizTimerDiv.appendChild(leaderBoard);
+    quizTimerDiv.removeChild(quizTimerTimesUp);
+    quizAnswersDiv.appendChild(playAgainButton);
+    // append the table to the leaderboard
+    quizQuestionDiv.appendChild(table);
+
+    var startTable = localStorage.getItem("localGameCount");
+
+    for(var i = startTable; i < scores.length; i++) {
+        var tr = document.createElement("tr");
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+
+        var text1 = document.createTextNode(scores[i][0].player);
+        var text2 = document.createTextNode(scores[i][0].highScore);
+
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        table.appendChild(tr);
+        console.log("looped through " + i + " times.");
+    }
+}
+
+function playAgain(){
+    console.log("play again!");
+    quizTimerDiv.appendChild(quizTimerP);
+    quizQuestionDiv.appendChild(quizQuestionP);
+    quizAnswersDiv.appendChild(option1);
+    quizAnswersDiv.appendChild(option2);
+    quizAnswersDiv.appendChild(option3);
+    quizAnswersDiv.appendChild(option4);
+
+    localStorage.setItem("localGameCount", ++gameCount);
+
+    quizTimerDiv.removeChild(leaderBoard);
+    quizQuestionDiv.removeChild(table);
+    quizAnswersDiv.removeChild(playAgainButton);
+
+    localStorage.setItem("localTimer", timer);
+    localStorage.setItem("localQuestionIndex", 0);
+
+    playGame();
+}
